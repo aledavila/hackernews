c506ae8b6271391a83852b3795f80cab6661a444
Alessandra Davila <aledavila@ibm.com>
2017-04-25 15:56:50 -0500

    adds search functionality

--- src/App.js
+++ src/App.js
@@ -28,28 +28,37 @@ class App extends Component {
     this.state = {
       list,
     };
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectId !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
   }
 
   render() {
     return (
       <div className="App">
-          { this.state.list.map(item =>
-                <div key={item.objectID}>
-                  <span>
-                    <a href={item.url}>{item.title}</a>
-                  </span>
-                  <span>{item.author}</span>
-                  <span>{item.num_comments}</span>
-                  <span>{item.points}</span>
-                  <span>
-                    <button onClick={() =>
-                              this.onDismiss(item.objectID)}
-                              type="button">
-                        Dismiss
-                    </button>
-                  </span>
-                </div>
-          )}
+        <form>
+          <input type="text" />
+        </form>
+        { this.state.list.map(item =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button onClick={() => this.onDismiss(item.objectID)} type="button">
+                Dismiss
+              </button>
+            </span>
+          </div>
+        )}
       </div>
     );
   }
