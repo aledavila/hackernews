69b1d6cffecb693290da91fb8d0bc4a2bdd8445e
Alessandra Davila <aledavila@ibm.com>
2017-04-25 17:31:03 -0500

    split components

--- src/App.js
+++ src/App.js
@@ -20,31 +20,36 @@ const list = [
   },
 ];
 
-class App extends Component {
+const isSearched = (searchTerm) => (item) =>
+  !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
+function isSearched(searchTerm) {
+  return function(item) {
+    return !searchTerm ||
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      list,
-    };
-
-    this.onDismiss = this.onDismiss.bind(this);
+    item.title.toLowerCase().includes(searchTerm.toLowerCase());
   }
+}
 
-  onDismiss(id) {
-    const isNotId = item => item.objectId !== id;
-    const updatedList = this.state.list.filter(isNotId);
-    this.setState({ list: updatedList });
-  }
-
+class Search extends Component {
   render() {
+    const { value, onChange } = this.props;
+    return() (
+      <form>
+        <input type="text"
+               value={value}
+               onChange={onChange}
+         />
+      </form>
+    )
+  }
+}
+
+class Table extends Component {
+  render() {
+    const {list, pattern, onDismiss } = this.props;
     return (
-      <div className="App">
-        <form>
-          <input type="text" />
-        </form>
-        { this.state.list.map(item =>
+      <div>
+        { list.filter(isSearched(pattern)).map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -59,6 +64,44 @@ class App extends Component {
             </span>
           </div>
         )}
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+      searchTerm: '',
+    };
+
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectId !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
+  render() {
+    const { searchTerm, list } = this.state;
+    return (
+      <div className="App">
+        <Search value={searchTerm}
+                onChange={this.onSearchChange} />
+        <Table list={list}
+               pattern={searchTerm}
+               onDismiss={this.onDismiss} />
       </div>
     );
   }
