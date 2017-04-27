6c8530dbea7b9eab05d65378ba84571e08102834
Alessandra Davila <aledavila@ibm.com>
2017-04-27 09:34:28 -0500

    adds title to columns

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 const DEFAULT_QUERY = 'react';
+const DEFAULT_PAGE = 0;
+const DEFAULT_HPP = '100';
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
-
-// const isSearched = (searchTerm) => (item) =>
-//     !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
 const Search = ({ value, onChange, children, onSubmit }) =>
   <form onSubmit={onSubmit}>
@@ -22,6 +23,22 @@ const Search = ({ value, onChange, children, onSubmit }) =>
 
 const Table = ({ list, onDismiss }) =>
   <div className="table">
+    <div className="table-header">
+      <span style={{ width: '40%' }}>
+        Title
+      </span>
+      <span style={{ width: '30%' }}>
+        Author
+      </span>
+      <span style={{ width: '10%' }}>
+        Comments
+      </span>
+      <span style={{ width: '10%' }}>
+        Points
+      </span>
+      <span style={{ width: '10%' }}>
+      </span>
+    </div>
     {list.map(item =>
       <div key={item.objectID} className="table-row">
         <span style={{ width: '40%' }}>
@@ -71,18 +88,31 @@ class App extends Component {
   }
 
   setSearchTopstories(result) {
-    this.setState({ result });
+    const { hits, page } = result;
+
+    const oldHits = page !== 0
+      ? this.state.result.hits
+      : [];
+
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ];
+
+    this.setState({
+      result: { hits: updatedHits, page }
+    });
   }
 
-  fetchSearchTopstories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopstories(searchTerm, page) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopstories(result));
   }
 
   componentDidMount() {
     const { searchTerm } = this.state;
-    this.fetchSearchTopstories(searchTerm);
+    this.fetchSearchTopstories(searchTerm, DEFAULT_PAGE);
   }
 
   onSearchChange(event) {
@@ -91,7 +121,7 @@ class App extends Component {
 
   onSearchSubmit(event) {
     const { searchTerm } = this.state;
-    this.fetchSearchTopstories(searchTerm);
+    this.fetchSearchTopstories(searchTerm, DEFAULT_PAGE);
     event.preventDefault();
   }
 
@@ -105,22 +135,27 @@ class App extends Component {
 
   render() {
     const { result, searchTerm } = this.state;
-
-    if (!result) { return null; }
+    const page = (result && result.page) || 0;
 
     return (
       <div className="page">
+        <h1>Hacker News</h1>
         <div className="interactions">
           <Search value={searchTerm}
                   onChange={this.onSearchChange}
                   onSubmit={this.onSearchSubmit} >
             Search
           </Search>
-          { result
-            ? <Table list={result.hits}
-                     onDismiss={this.onDismiss} />
-            : null
-          }
+        </div>
+        { result &&
+          <Table list={result.hits}
+                 onDismiss={this.onDismiss} />
+        }
+        <div className="interactions">
+          <Button onClick={() =>
+              this.fetchSearchTopstories(searchTerm, page + 1)}>
+            More
+          </Button>
         </div>
       </div>
     );
